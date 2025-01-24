c05cc93cac16b4f39003e736b617a62a9b8dd071
_ <layaniop9@gmail.com>
2025-01-25 01:11:19 +0300

    Add files via upload

---
+++ CircularDoublyLinkedList.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package project;
+
+/**
+ *
+ * @author Admin
+ */
+public class CircularDoublyLinkedList<E> {
+    
+private Node<E> header;
+private Node<E> trailer;
+private int size = 0;  
+
+public int size(){ return size; }
+
+    
+    
+}
