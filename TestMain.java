0d035321b2c6c6261f8bd967a7c5042a9aa1fc32
Retajkhaleed <RetajKhaleed05@gmail.com>
2025-01-25 18:31:01 +0300

    Update TestMain.java

--- TestMain.java
+++ TestMain.java
@@ -4,20 +4,20 @@ public class TestMain {
     public static void main(String[] args) {
         CircularDoublyLinkedList<Integer> list = new CircularDoublyLinkedList<>();
 
-        list.add(10);
-        list.add(20);
-        list.add(30);
-        list.add(40);
+        list.addFirst(10);
+        list.addFirst(20);
+        list.addFirst(30);
+        list.addFirst(40);
 
         System.out.println("List is :");
         list.print();
 
         list.rotate();
-        System.out.println("List after rotation:");
+        System.out.println("List after forward rotation:");
         list.print();
 
-        list.rotate();
-        System.out.println("List after another rotation:");
+        list.rotateBackward();
+        System.out.println("List after backward rotation:");
         list.print();
 
         System.out.println("Is the list empty? " + list.isEmpty());
